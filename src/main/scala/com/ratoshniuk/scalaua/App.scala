76eaca81fe4817f3cbc5786c36c6b68acddee18d
ratoshniuk <mratoshniuk@gmail.com>
2019-03-30 01:17:05 +0200

    postgres sink

--- src/main/scala/com/ratoshniuk/scalaua/App.scala
+++ src/main/scala/com/ratoshniuk/scalaua/App.scala
@@ -5,20 +5,26 @@ import java.util.concurrent.{Executors, ThreadPoolExecutor}
 import com.github.pshirshov.izumi.functional.bio.{BIO, BIOAsync, BIORunner}
 import com.ratoshniuk.scalaua.YourService.{AdsPlatform, UserId}
 import scalaz.zio.IO
-
+import cats.effect._
+import scala.concurrent.ExecutionContext
 import scala.util.Random
 
 object ScalaUA extends App {
 
   import logstage._
 
-  implicit val bioRunner = BIORunner.createZIO(
+  implicit lazy val blockingExecutor =  Executors.newCachedThreadPool()
+  implicit lazy val blockingEc =  ExecutionContext.fromExecutor(blockingExecutor)
+
+  implicit lazy val csh = cats.effect.IO.contextShift(blockingEc)
+
+  implicit lazy val bioRunner = BIORunner.createZIO(
     Executors.newFixedThreadPool(8).asInstanceOf[ThreadPoolExecutor]
-    , Executors.newCachedThreadPool().asInstanceOf[ThreadPoolExecutor]
+    , blockingExecutor.asInstanceOf[ThreadPoolExecutor]
   )
 
 //  val textSink = ConsoleSink.text(colored = true)
-  val jsonSink = new DBJsonSink()
+  val jsonSink = new DBJsonSink(new PostgresConnector(blockingEc))
 
 //  val sinks = List(textSink)
   val sinks = List(jsonSink)
--- src/main/scala/com/ratoshniuk/scalaua/DBJsonSink.scala
+++ src/main/scala/com/ratoshniuk/scalaua/DBJsonSink.scala
@@ -3,35 +3,87 @@ package com.ratoshniuk.scalaua
 import java.util.concurrent.atomic.{AtomicInteger, AtomicReference}
 import java.util.concurrent.{ConcurrentLinkedDeque, ConcurrentLinkedQueue}
 
-import com.github.pshirshov.izumi.functional.bio.{BIORunner, BlockingIO}
+import cats.effect.ContextShift
+import com.github.pshirshov.izumi.functional.bio.{BIO, BIOAsync, BIORunner, BlockingIO}
 import com.github.pshirshov.izumi.logstage.api.Log
+import BIO._
+import cats.data.NonEmptyList
 import com.github.pshirshov.izumi.logstage.api.logger.LogSink
 import com.github.pshirshov.izumi.logstage.api.rendering.json.LogstageCirceRenderingPolicy
+import com.zaxxer.hikari.{HikariConfig, HikariDataSource}
+import doobie.util.update.Update
 import logstage.circe.LogstageCirce
 import scalaz.zio.IO
 
 import scala.collection.mutable.ListBuffer
 
-class DBJsonSink(
-//                  blockingIO: BlockingIO[IO]
-                )(implicit BIORunner: BIORunner[IO]) extends LogSink {
+class DBJsonSink
+(
+  pgConnector: PostgresConnector[IO]
+)(implicit BIORunner: BIORunner[IO]) extends LogSink {
 
   val policy = new LogstageCirceRenderingPolicy(false)
 
   val limit = 10
 
-  val msgsAtomic = new AtomicReference[ListBuffer[String]](ListBuffer.empty[String])
+  val msgsAtomic = new AtomicReference[ListBuffer[(Long, String)]](ListBuffer.empty[(Long, String)])
 
   override def flush(e: Log.Entry): Unit = {
     val jsonified = policy.render(e)
     val msgs = msgsAtomic.get()
     if (msgs.size == limit) {
       val beforeSend = msgs.takeRight(limit).toList
+      BIORunner.unsafeRun(insertIntoDB(beforeSend))
       msgsAtomic.set(msgs -- beforeSend)
-//      BIORunner.unsafeRun()
     } else {
-      msgs += jsonified
+      msgs += Tuple2(e.context.dynamic.tsMillis, jsonified)
       msgsAtomic.set(msgs)
     }
   }
+
+  private def insertIntoDB(entries : List[(Long, String)]) : IO[Nothing, Unit] = {
+    import doobie.implicits._
+    val q = Update[(Long, String)]("""INSERT INTO "public"."structured_logs"("timestamp", "log") VALUES(TO_TIMESTAMP(?::double precision / 1000), jsonb(?))""".stripMargin).updateMany(NonEmptyList.fromListUnsafe(entries))
+    pgConnector.query(q).void
+  }
+}
+
+import doobie.free.connection.ConnectionIO
+import doobie.hikari.HikariTransactor
+import doobie.syntax.connectionio._
+import logstage.IzLogger
+
+import scala.concurrent.ExecutionContext
+import scala.concurrent.duration._
+
+final class PostgresConnector[F[+_, +_]: BIO: BIOAsync]
+(
+  blockingIOExecutionContext: ExecutionContext
+)(implicit cs: ContextShift[cats.effect.IO])    {
+
+  import scala.concurrent.duration._
+
+  def query[T](query: ConnectionIO[T]): F[Nothing, T] = {
+    for {
+      res <- {
+        BIO[F].syncThrowable(query.transact(mkTransactor).unsafeRunSync())
+          .catchAll(f => BIO[F].terminate(f))
+      }
+    } yield res
+  }
+
+  private[this] lazy val mkTransactor: HikariTransactor[cats.effect.IO] = {
+    val ds = new HikariDataSource(cfg)
+    HikariTransactor.apply[cats.effect.IO](ds, blockingIOExecutionContext, blockingIOExecutionContext)
+  }
+
+  private val cfg : HikariConfig = {
+    val config = new HikariConfig()
+    config.setJdbcUrl("jdbc:postgresql://localhost/postgres")
+    config.setUsername("postgres")
+    config.setPassword("postgres")
+    config.setDriverClassName("org.postgresql.Driver")
+    config
+  }
+
 }
