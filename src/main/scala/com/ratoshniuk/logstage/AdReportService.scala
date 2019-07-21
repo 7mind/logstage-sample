7970528a2b7c13e1d62d4b707f80e0e12d1408f1
ratoshniuk <mratoshniuk@gmail.com>
2019-07-21 16:32:04 +0300

    wip

--- src/main/scala/com/ratoshniuk/logstage/AdReportService.scala
+++ src/main/scala/com/ratoshniuk/logstage/AdReportService.scala
@@ -2,29 +2,47 @@ package com.ratoshniuk.logstage
 
 import com.github.pshirshov.izumi.functional.bio.BIOAsync
 import com.github.pshirshov.izumi.logstage.api.IzLogger
+import com.ratoshniuk.logstage.AdReportService.AdPlatform.BadGuys
 import com.ratoshniuk.logstage.AdReportService.{AdPlatform, UserId}
 import zio.duration.Duration.Finite
-import zio.{DefaultRuntime, IO, Schedule, ZIO}
+import zio.{IO, Schedule}
 
 import scala.concurrent.duration._
 import scala.util.Random
+import com.ratoshniuk.logstage.AdReportService.AdPlatform.BadGuys
+import logstage.LogBIO
+import zio.clock.Clock
+import zio.{IO, Schedule, ZIO}
 
 class AdReportService(logger: IzLogger)(implicit async: BIOAsync[IO])
 {
-  def pullReports(userId: UserId, adPlatform: AdPlatform) : ZIO[DefaultRuntime#Environment, Nothing, Unit] = {
+  def pullReports(userId: UserId, adPlatform: AdPlatform) : ZIO[Any with Clock, Nothing, Unit] = {
+    val granularLog = LogBIO.fromLogger[IO](
+      logger(
+        "userId" -> userId.value,
+        "ad" -> adPlatform.toString
+      )
+    )
     for {
-      _        <- IO.succeed(logger.info(s"start pulling for $userId amd $adPlatform"))
-      readIO    = (readFromNetwork(userId, adPlatform) repeat  processRule).unit
-      writeIO   = (putIntoS3(adPlatform) repeat  processRule).unit
-      _        <- zio.ZIO.raceAll(readIO, List(writeIO))
+      _        <- granularLog.info(s"start processing batching")
+      readIO    = (readFromNetwork(userId, adPlatform, granularLog) repeat  processRule).unit
+      writeIO   = (putIntoS3(adPlatform, granularLog) repeat  processRule).unit
+      _        <- zio.ZIO.raceAll(readIO, Seq(writeIO))
+      _        <- granularLog.info(s"batch processing completed")
     } yield ()
   }
-  def readFromNetwork(userId: UserId, adPlatform: AdPlatform) :  IO[Nothing, Unit] = {
-    async.sleep(5.seconds).unit
+  def readFromNetwork(userId: UserId, adPlatform: AdPlatform, log: LogBIO[IO]) :  IO[Nothing, Unit] = {
+    adPlatform match {
+      case BadGuys => async.sleep(0.3.seconds) *> log.error(s"Rate limit exceeded")
+      case _       => async.sleep(1.second)    *> log.info(s"Received ${2 * 1000 -> "rows"}")
+    }
   }
 
-  def putIntoS3(adPlatform: AdPlatform) : IO[Nothing, Unit] = {
-    async.sleep(10.seconds).unit
+  def putIntoS3(adPlatform: AdPlatform, log: LogBIO[IO]) : IO[Nothing, Unit] = {
+    adPlatform match {
+      case BadGuys => async.sleep(0.3.seconds) *> log.warn(s"Uploading to S3 Skipped")
+      case _       => async.sleep(1.second)    *> log.info(s"Received ${2 * 1000 -> "rows"}")
+    }
   }
 
   private val processRule = Schedule.spaced(Finite.apply(5.seconds.toNanos))
